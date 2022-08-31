f7381f48d7c38da797a9a103116c224d45f5beef
João Vitor Viana de Lima <viana.lima@academico.ifrn.edu.br>
2022-08-31 16:38:42 -0300

    comentario

--- core/models.py
+++ core/models.py
@@ -1,3 +1,16 @@
 from django.db import models
 
-# Create your models here.
+class Alunos(models.Model):
+    nome = models.CharField('Título', max_length=100)
+    numero = models.CharField('Título', max_length=11)
+    cpf = models.CharField('Título', max_length=11)
+
+class Funcionarios(models.Model):
+    nome = models.CharField('Título', max_length=100)
+    numero = models.CharField('Título', max_length=11)
+    cref = models.CharField('Título', max_length=6)
+
+class Maquinas(models.Model):
+    nome = models.CharField('Título', max_length=100)
+    numero = models.CharField('Título', max_length=11)    
+    
